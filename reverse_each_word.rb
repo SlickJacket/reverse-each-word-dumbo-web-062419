f548375cbbadc3e2b79ff4467180eb2a74f5e9ce
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-15 17:03:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  array = [string]
+  array = string.split(" ")
   array2 = []
-  array.each |n|
-  array2 = n.reverse
+  array.each do|string|
+  array2 << n.reverse
 end
